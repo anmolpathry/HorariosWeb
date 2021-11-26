41549aad2ebb93d4fa90c3a717da91c78d5705c7
anmolpathry <anmolitasweet@gmail.com>
2021-11-26 15:28:28 -0600

    admin_classes completo
    También moví todo a carpetas y empece a poner estructura de cada Clase

---
+++ Controllers/classes.js
@@ -0,0 +1,26 @@
+"use strict";
+
+class Classes {  
+    //constructor y atributos
+    constructor() {      
+    }
+
+    //getters y setters
+
+
+    //métodos
+    //createClassFromObject()
+
+    //createClassFromJSON()
+
+    //cleanClass()
+
+    //updateClass()
+
+    //deleteClass()
+
+    //getClassById()
+
+
+    
+}
---
+++ Controllers/subject.js
@@ -0,0 +1,27 @@
+"use strict";
+
+class Subject {  
+    //constructor y atributos
+    constructor() {      
+    }
+
+    //getters y setters
+
+
+    //métodos
+    //createSubjectFromObject()
+
+    //createSubjectFromJSON()
+
+
+    //cleanSubject()
+
+    //updateSubject()
+
+    //deleteSubject()
+
+    //getClassesById()
+
+    //getSubjectById()
+    
+}
