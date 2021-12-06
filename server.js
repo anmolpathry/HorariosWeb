9f2c51fa581a480c641e87b0e2a72b33fc178883
anmolpathry <anmolitasweet@gmail.com>
2021-12-05 23:54:28 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,11 @@ const express = require('express');
 const cors = require('cors');
 const router = require("./Controllers/router");
 const app = express();
-const port = 8080;
+//const port = 8080;
+
+app.set('port', process.env.PORT || 3000);
+app.use(router);
+app.listen(app.get('port'),process.env.HOST)
 
 app.use(cors());
 app.use(express.json()); 
