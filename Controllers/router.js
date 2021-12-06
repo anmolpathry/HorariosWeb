a9470656ef8886c75f096a9871007192f1685d13
anmolpathry <anmolitasweet@gmail.com>
2021-12-05 23:43:36 -0600

    agregar ruta login

--- Controllers/router.js
+++ Controllers/router.js
@@ -72,6 +72,10 @@ router.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, "../AJAX/login.html"));
 });
 
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, "../AJAX/login.html"));
+});
+
 router.get('/signup', (req, res) => {
     res.sendFile(path.join(__dirname, "../AJAX/signup.html"));
 });
